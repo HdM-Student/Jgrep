7a8734841e8614878fabc577cd396dd99cfb30d1
Fuzzel <linard.hug@googlemail.com>
2016-06-03 15:56:28 +0200

    Fixed -l argument output bug

--- maven/src/test/java/de/linard/JGrep/AppTest.java
+++ maven/src/test/java/de/linard/JGrep/AppTest.java
@@ -6,12 +6,15 @@ import org.junit.Test;
  * Unit test for simple App.
  */
 public class AppTest {
-    /**
-     * Dummy test method
-     */
-    @Test
-    public void testApp() {
-    	String[] args = {"flower", "./target/test-classes/input.txt"};
-        new Jgrep(args);
-    }
+	/**
+	 * Dummy test method
+	 */
+	@Test
+	public void testApp() {
+		new Jgrep("flower ./target/test-classes/input.txt".split(" "));
+		new Jgrep("-i red ./target/test-classes/input.txt".split(" "));
+		new Jgrep("-i RED ./target/test-classes/input.txt ./target/test-classes/secondInput.txt".split(" "));
+		new Jgrep("-l Red ./target/test-classes/input.txt ./target/test-classes/secondInput.txt".split(" "));
+		new Jgrep("-l -i Red ./target/test-classes/input.txt ./target/test-classes/secondInput.txt".split(" "));
+	}
 }
